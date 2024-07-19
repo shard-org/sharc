9cae06255c7b7dd85401f7b3d9e3bcc7d7a7db59
Haven Selph <havenselph@gmail.com>
2024-07-19 16:20:10 -0500

    added span printer

--- src/span.rs
+++ src/span.rs
@@ -4,7 +4,6 @@ use std::fmt::Formatter;
 pub struct Span {
     pub filename: &'static str,
     pub line_number: usize,
-    pub line_index: usize,
     pub start_index: usize,
     pub end_index: usize,
 }
@@ -13,14 +12,12 @@ impl Span {
     pub fn new(
         filename: &'static str,
         line_number: usize,
-        line_index: usize,
         start_index: usize,
         end_index: usize,
     ) -> Self {
         Self {
             filename,
             line_number,
-            line_index,
             start_index,
             end_index,
         }
@@ -30,16 +27,31 @@ impl Span {
         Self {
             filename: self.filename,
             line_number: self.line_number,
-            line_index: self.line_index,
             start_index: self.start_index,
             end_index: other.end_index,
         }
     }
+
+    pub fn to_span_printer(&self, line_index: usize) -> SpanPrinter {
+        SpanPrinter {
+            span: self,
+            line_index,
+        }
+    }
 }
 
-impl std::fmt::Display for Span {
+pub struct SpanPrinter<'s> {
+    span: &'s Span,
+    line_index: usize,
+}
+
+impl std::fmt::Display for SpanPrinter {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let column = self.start_index - self.line_index;
-        write!(f, "{}:{}:{}", self.filename, self.line_number, column)
+        let column = self.span.start_index - self.line_index;
+        write!(
+            f,
+            "{}:{}:{}",
+            self.span.filename, self.span.line_number, column
+        )
     }
 }
