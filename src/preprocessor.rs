6e8cb54835a0be81bdaa5d252e1d7379d68e0c00
slbsh <crafter.antek@gmail.com>
2024-07-31 21:50:11 +0200

    feat: preprocessor finally working!

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -4,7 +4,10 @@ use crate::scanner::Scanner;
 use crate::span::Span;
 use crate::token::{Token, TokenKind};
 use crate::ast::{Type};
-// use crate::span::Span;
+
+// use index_list::{IndexList, ListIndex};
+use std::collections::VecDeque;
+// ! USE LINKED LISTTTTTTT
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Tag {
@@ -16,129 +19,123 @@ pub enum Tag {
     // Syscall(Vec<Box<AST>>, String), // expect TypeAnnotation
 }
 
+#[derive(Debug, Clone)]
+enum TokenWrap<'contents> {
+    Token(Token<'contents>),
+    Macro(String),
+}
+
+impl<'contents> TokenWrap<'contents> {
+    fn token(&self) -> Option<&Token<'contents>> {
+        match self {
+            TokenWrap::Token(token) => Some(token),
+            TokenWrap::Macro(_) => None,
+        }
+    }
+
+    fn to_macro(&self) -> Option<&str> {
+        match self {
+            TokenWrap::Token(_) => None,
+            TokenWrap::Macro(name) => Some(name),
+        }
+    }
+}
+
 pub struct PreProcessor<'contents> {
     filename:   &'static str,
-    tokens:     std::iter::Peekable<std::vec::IntoIter<Token<'contents>>>,
+    tokens:     VecDeque<Token<'contents>>,
+    index:      usize,
     sender:     ReportSender,
 
-    output:     Vec<Token<'contents>>,
-    tags:       HashSet<Tag>,
-
-    macro_defs: HashMap<String, Vec<Token<'contents>>>,
+    tag_defs:   HashMap<String, (Span, Vec<TokenWrap<'contents>>)>,
+    macro_defs: HashMap<String, Vec<TokenWrap<'contents>>>,
 }
 
 impl<'contents> PreProcessor<'contents> {
     pub fn new(filename: &'static str, tokens: Vec<Token<'contents>>, sender: ReportSender) -> Self {
-        let tokens_len = tokens.len();
-        let mut tokens = tokens.into_iter().peekable();
-
         Self {
-            filename, tokens, sender,
-            output: Vec::with_capacity(tokens_len),
-            tags: HashSet::new(),
+            filename, sender, 
+            tokens: tokens.into(),
+            index: 0,
+            // tags: HashSet::new(),
+            tag_defs: HashMap::new(),
             macro_defs: HashMap::new(),
         }
     }
 
-    fn report(&mut self, report: Box<Report>) {
-        self.sender.send(report)
+    fn advance(&mut self) {
+        self.index += 1;
     }
 
-    fn next(&mut self) -> Result<Token<'contents>> {
-        self.tokens.next().ok_or_else(|| {
-            let file = Scanner::get_file(self.filename);
-
-            let line_number = file.lines().count();
-
-            ReportKind::UnexpectedEOF
-                .new("Unexpected end of file")
-                .with_label(ReportLabel::new(
-                    Span::new(self.filename, line_number, file.len()-1, file.len())
-                )).into()
-        })
+    fn consume(&mut self) -> Token<'contents> {
+        self.tokens.remove(self.index).unwrap()
     }
 
-    fn advance(&mut self) -> Result<()> {
-        let token = self.next()?;
-        self.output.push(token);
-        Ok(())
+    fn current(&self) -> &Token<'contents> {
+        &self.tokens[self.index]
     }
 
-    fn peek(&mut self) -> &Token<'contents> {
-        self.tokens.peek().unwrap()
+    fn peek(&self) -> &Token<'contents> {
+        &self.tokens[self.index + 1]
+    }
+
+    fn parse_token(&mut self) -> TokenWrap<'contents> {
+        let token = self.consume();
+        match token.kind {
+            TokenKind::Pound
+                if self.current().kind == TokenKind::Identifier => {
+                    let token = self.consume();
+                    TokenWrap::Macro(token.text.to_string())
+                },
+            _ => TokenWrap::Token(token),
+        }
     }
 
     pub fn process(mut self) -> (Vec<Token<'contents>>, HashSet<Tag>) {
-        match self.index_all().and_then(|_| self.macro_expand(1)) {
-            Ok(()) => (self.output, self.tags),
-            Err(err) => {
-                self.report(err);
-                (Vec::new(), HashSet::new())
-            }
+        match self.macro_processor()
+            .map(|_| unsafe{self.expand_macro_defs()})
+            .map(|_| self.expand_tag_defs())
+            .and_then(|_| self.expand_macros())
+            .and_then(|tokens| self.into_tags_map().map(|tags| (tokens, tags)))
+            // .map_err(|err| self.sender.send(err))
+            {
+            Ok(t) => {
+                println!("\nMACRO-DEFS:");
+                self.macro_defs.iter().for_each(|(k, v)| {
+                    let v = v.iter().fold(String::new(), |mut acc, t| {
+                        acc = acc + &match t {
+                            TokenWrap::Macro(m) => format!("Macro({:?})", m),
+                            TokenWrap::Token(t) => format!("{:?} ", t.text),
+                        };
+                        acc
+                    });
+                    println!("{}: {}", k, v);
+                });
+                t
+            },
+            Err(_) => (Vec::new(), HashSet::new())
         }
     }
 
-    fn macro_expand(&mut self, pass: usize) -> Result<()> {
-        if pass > 10 {
-            return ReportKind::ExceededRecursionLimit
-                .new("reached over 10 macro expansion passes")
-                .into();
-        }
-
-        let mut index = 0;
-        let mut is_done = true;
-
-        while let token = &self.output[index] {
-            index += 1;
-            match token.kind {
-                TokenKind::Pound
-                    if self.output[index].kind == TokenKind::Identifier => {
-                        let ident = self.output[index].text;
-                        match self.macro_defs.get(ident) {
-                            Some(tokens) => {
-                                is_done = false;
-                                self.output.splice(index-1..index+1, tokens.iter().cloned());
-                            },
-                            None => return ReportKind::UndefinedMacro
-                                .new(format!("Undefined macro: {ident}"))
-                                .with_label(ReportLabel::new(token.span.clone()))
-                                .into(),
-                        }
-                    },
-                TokenKind::EOF => break,
-                _ => {},
-            }
-        }
-
-        if !is_done {
-            self.macro_expand(pass + 1)?;
-        }
-
-        Ok(())
-    }
-
-    fn index_all(&mut self) -> Result<()> {
+    fn macro_processor(&mut self) -> Result<()> {
         let mut is_line_start = true;
 
-        while let token = self.next()? {
-            match token.kind {
-                TokenKind::EOF => {
-                    self.output.push(token);
-                    break
-                },
+        loop {
+            match self.current().kind {
+                TokenKind::EOF => break,
                 TokenKind::Colon if is_line_start => {
                     self.index_tag()?;
                     is_line_start = true;
                 },
                 TokenKind::NewLine 
                     if self.peek().kind == TokenKind::Colon => {
-                        self.next()?;
+                        self.consume();
                         self.index_tag()?;
                         is_line_start = true;
                     },
                 _ => {
                     is_line_start = false;
-                    self.output.push(token)
+                    self.advance();
                 },
             }
         }
@@ -146,70 +143,167 @@ impl<'contents> PreProcessor<'contents> {
     }
 
     fn index_tag(&mut self) -> Result<()> {
-        let token = self.next()?;
-        match token.text.to_lowercase().as_str() {
-            "name" => {
-                let token = self.next()?;
-                if token.kind != TokenKind::StringLiteral {
-                    return ReportKind::SyntaxError
-                        .new(format!("Expected StringLiteral; got {:?}", token.kind))
-                        .with_label(ReportLabel::new(token.span))
-                        .into();
-                }
-
-                self.tags.insert(Tag::Name(token.text.to_string()));
-                self.macro_defs.insert(String::from("NAME"), vec![token]);
-                Ok(())
-            },
-
-            "arch" => {
-                let mut arch = Vec::with_capacity(3);
-                while let token = self.next()? {
-                    match token.kind {
-                        TokenKind::NewLine => break,
-                        TokenKind::Identifier => arch.push(token),
-                        _ => return ReportKind::SyntaxError
-                            .new("Expected identifier")
-                            .with_label(ReportLabel::new(token.span))
-                            .into(),
-                    }
-                }
-
-                self.tags.insert(Tag::Arch(arch.clone().into_iter().map(|token| token.text.to_string()).collect()));
-
-                arch.iter_mut().for_each(|token| token.kind = TokenKind::StringLiteral);
-                self.macro_defs.insert(String::from("ARCH"), arch);
-                Ok(())
-            },
-
-            "macro" => {
-                let token = self.next()?;
-                if token.kind != TokenKind::Identifier {
-                    return ReportKind::SyntaxError
-                        .new("Expected identifier")
-                        .with_label(ReportLabel::new(token.span))
-                        .into();
-                }
-
-                let name = token.text.to_string();
-                let mut tokens = Vec::new();
-                while let token = self.next()? {
-                    match token.kind {
-                        TokenKind::NewLine => break,
-                        _ => tokens.push(token),
-                    }
-                }
-
-                self.macro_defs.insert(name.to_string(), tokens);
-                self.tags.insert(Tag::Macro(name));
-                Ok(())
-            },
-
-            text => ReportKind::InvalidTag
-                .new(format!("{text:?}"))
-                .with_label(ReportLabel::new(token.span.clone()))
-                .into(),
+        self.consume();
+        let init_token = self.consume();
+        if init_token.kind != TokenKind::Identifier {
+            return ReportKind::SyntaxError
+                .new(format!("Expected identifier; got {:?}", init_token.kind))
+                .with_label(ReportLabel::new(init_token.span))
+                .into();
         }
+
+        let mut args = Vec::with_capacity(4);
+        while self.current().kind != TokenKind::NewLine {
+            args.push(self.parse_token());
+        }
+
+        match init_token.text {
+            "name" | "arch" => { 
+                self.tag_defs.insert(init_token.text.to_uppercase(), (init_token.span, args.clone()));
+                self.macro_defs.insert(init_token.text.to_uppercase(), args);
+            },
+            "macro" => {
+                match args.get(0).and_then(|t| t.token()) {
+                    Some(token) if token.kind != TokenKind::Identifier => {
+                        return ReportKind::SyntaxError
+                            .new(format!("Expected Identifier; got {:?}", token.kind))
+                            .with_label(ReportLabel::new(token.span.clone()))
+                            .into();
+                    },
+                    Some(token) => self.add_macro_def(token.text, args[1..].to_vec()),
+                    None => return ReportKind::SyntaxError
+                        .new("Expected Identifier")
+                        .with_label(ReportLabel::new(init_token.span))
+                        .into(),
+                }
+            },
+            _ => { self.tag_defs.insert(init_token.text.to_uppercase(), (init_token.span, args)); },
+        }
+        Ok(())
+        // _ => ReportKind::InvalidTag
+        //     .new(format!("{:?}", token.text))
+        //     .with_label(ReportLabel::new(token.span.clone()))
+        //     .into(),
+    }
+
+    fn add_macro_def(&mut self, name: &str, mut tokens: Vec<TokenWrap<'contents>>) {
+        // if macro already exists, replace all instances in `tokens` and set it as the new definition
+        if let Some(existing) = self.macro_defs.get_mut(name) {
+            let mut index = 0;
+            while let Some(i) = tokens[index..].iter().position(|t| t.to_macro().is_some_and(|n| n == name)){
+                tokens.splice(index+i..index+i+1, existing.iter().cloned());
+                index += i + 1;
+            }
+        }
+
+        // if `tokens` has a macro already defined, replace with current definition
+        let mut index = 0;
+        while let Some(i) = tokens[index..].iter().position(|t| t.to_macro().is_some()) {
+            if let Some(existing) = tokens[index+1-1].to_macro().and_then(|name| self.macro_defs.get(name)) {
+                tokens.splice(index+i..index+i+1, existing.iter().cloned());
+            }
+            index += i + 1;
+        }
+
+        self.macro_defs.insert(name.to_string(), tokens);
+    }
+
+    unsafe fn expand_macro_defs(&mut self) {
+        self.macro_defs.keys().cloned().collect::<Vec<_>>().iter().for_each(|key| {
+            let tokens = self.macro_defs.get_mut(key).unwrap() as *mut Vec<TokenWrap<'contents>>;
+            let mut index = 0;
+            while let Some(i) = (*tokens)[index..].iter().position(|t| t.to_macro().is_some()) {
+                if let Some(existing) = (*tokens)[index+i].to_macro().and_then(|n| self.macro_defs.get(n)) {
+                    (*tokens).splice(index+i..index+i+1, existing.iter().cloned());
+                }
+                index += i + 1; 
+            }
+        })
+    }
+
+    fn expand_tag_defs(&mut self) {
+        self.tag_defs.iter_mut().for_each(|(_, (_, tokens))| {
+            let mut index = 0;
+            while let Some(i) = tokens[index..].iter().position(|t| t.to_macro().is_some()) {
+                if let Some(existing) = tokens[index+i].to_macro().and_then(|n| self.macro_defs.get(n)) {
+                    tokens.splice(index+i..index+i+1, existing.iter().cloned());
+                }
+                index += i + 1; 
+            }
+        })
+    }
+
+    fn expand_macros(&mut self) -> Result<Vec<Token<'contents>>> {
+        let mut tokens = Vec::with_capacity(self.tokens.len());
+        while let Some(token) = self.tokens.pop_front() {
+            match token.kind {
+                TokenKind::EOF => {
+                    tokens.push(token);
+                    break;
+                },
+                TokenKind::Pound 
+                    if self.tokens.front().is_some_and(|t| t.kind == TokenKind::Identifier) => {
+                        let token = self.tokens.pop_front().unwrap();
+                        if let Some(def) = self.macro_defs.get(token.text) {
+                            tokens.extend(def.iter().map(|t| t.token().unwrap().clone()));
+                            continue;
+                        }
+
+                        return ReportKind::InvalidTag
+                            .new(format!("{:?}", token.text))
+                            .with_label(ReportLabel::new(token.span.clone()))
+                            .into();
+                    },
+                _ => tokens.push(token),
+            }
+        }
+        Ok(tokens)
+    }
+
+    fn into_tags_map(&self) -> Result<HashSet<Tag>> {
+        let mut tags = HashSet::new();
+
+        for (key, (span, tokens)) in self.tag_defs.iter() {
+            match key.as_str() {
+                "NAME" => {
+                    let name = match tokens.first().and_then(|t| t.token()){ 
+                        Some(token) 
+                            if token.kind == TokenKind::StringLiteral => token.text.to_string(),
+                        Some(token) => return ReportKind::SyntaxError
+                            .new(format!("Expected StringLiteral; got {:?}", token.kind))
+                            .with_label(ReportLabel::new(token.span.clone()))
+                            .into(),
+                        None => return ReportKind::SyntaxError
+                            .new("Expected StringLiteral")
+                            .with_label(ReportLabel::new(self.current().span.clone()))
+                            .into(),
+                    };
+
+                    if tokens.len() > 1 {
+                        return ReportKind::SyntaxError
+                            .new("Expected 1 argument")
+                            .with_label(ReportLabel::new(span.clone()))
+                            .into();
+                    }
+
+                    tags.insert(Tag::Name(name));
+                },
+                "ARCH" => {
+                    if let Some(token) = tokens.iter().map(|t| t.token().unwrap()).find(|t| t.kind != TokenKind::Identifier) {
+                        return ReportKind::SyntaxError
+                            .new("Expected Identifier")
+                            .with_label(ReportLabel::new(token.span.clone()))
+                            .into();
+                    }
+
+                    let arch = tokens.iter()
+                        .map(|t| t.token().unwrap().text.to_string())
+                        .collect::<Vec<_>>();
+                    tags.insert(Tag::Arch(arch));
+                },
+                _ => (),
+            }
+        }
+        Ok(tags)
     }
 }
-
